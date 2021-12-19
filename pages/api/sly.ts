78d889d3600bae8aebdf243be0673e36390f698b
Kenneth Duverge <kenneth.duverge@icloud.com>
2021-12-19 16:29:13 -0500

    Updates

--- pages/api/sly.ts
+++ pages/api/sly.ts
@@ -1,25 +1,19 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
-import fs from 'fs';
-import path from 'path';
-import { v2 as cloudinary } from 'cloudinary';
 
-type Data = {
+import { readFile } from 'fs/promises';
+
+interface Service {
   name: string;
-};
+  appointment: number;
+}
 
-const filePath = path.join(__dirname, 'data.json');
+interface Data {
+  data: Service[];
+}
 
-fs.readFile(filePath, 'utf-8', (data) => {
-  console.log(filePath);
-  console.log({ data });
-});
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  const data = await readFile('./public/data.json', 'utf-8');
 
-export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-  if (req.method === 'POST') {
-    console.log('Making a post request');
-    return res.status(200).json({ name: 'John Doe' });
-  }
-
-  return res.status(201).json({ name: 'Jane Doe' });
+  return res.status(200).json({ data: JSON.parse(data) });
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,16 @@
-import { useState } from 'react';
-
-import data from '../data.json';
+// import data from '../public/data.json';
 import { Service } from '../components/Service';
 
-const Home = () => {
+interface Service {
+  name: string;
+  appointment: number;
+}
+
+interface Props {
+  data: Service[];
+}
+
+const Home = ({ data }: Props) => {
   return (
     <section className="grid sm:p-5 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-2 gap-2 mt-10 w-full max-h-full">
       {data.map(({ name, appointment }, index) => (
@@ -13,7 +20,10 @@ const Home = () => {
   );
 };
 
-export const getServerSideProps = () => {
+export const getServerSideProps = async () => {
+  const response = await fetch('http://localhost:3000/api/sly');
+  const { data } = await response.json();
+
   return {
     props: {
       data,
