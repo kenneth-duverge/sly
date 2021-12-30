eda2f8dec3afcd06d9fa2bdfd9ffbfd943614fa0
Kenneth Duverge <kenneth.duverge@icloud.com>
2021-12-29 21:46:35 -0500

    Install react-dates & moment

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
 import 'tailwindcss/tailwind.css';
 import '../styles/globals.css';
+import 'react-dates/initialize';
+import 'react-dates/lib/css/_datepicker.css';
 
 import { ReactElement, ReactNode } from 'react';
 import { NextPage } from 'next';
